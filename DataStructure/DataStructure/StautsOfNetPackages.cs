6f21f9c4906eb8899384862a7c5f34d4ea57bd46
mmakarova <makarova.marina.andreevna@gmail.com>
2024-07-25 17:48:24 +0300

    Change tests

--- DataStructure/DataStructure/StautsOfNetPackages.cs
+++ DataStructure/DataStructure/StautsOfNetPackages.cs
@@ -8,22 +8,23 @@ using System.Threading.Tasks;
 namespace DataStructure {
     public class StautsOfNetPackages {
         Queue queue;
-        public StautsOfNetPackages(string size_number, List<string>? packages) {            
-            if (packages != null) {                
+        public StautsOfNetPackages(string size_number, List<string>? packages) {
+            if (packages != null) {
                 queue = new Queue(Convert.ToInt32(size_number.Split(' ')[0]));
                 for (int i = 0; i < packages.Count; i++) {
                     int arrivalTime = Convert.ToInt32(packages[i].Split(" ")[0]);
                     int durationTime = Convert.ToInt32(packages[i].Split(" ")[1]);
                     Package package = new Package() { ArrivalTime = arrivalTime, DurationTime = durationTime };
+                    queue.ProcessPackage();
                     queue.AddPackage(package);
                 }
-                foreach (Package p in queue.QueueList) {
-
+                for (int i = 0; i < queue.QueueList.Count; i++) {
+                    queue.ProcessPackage();
                 }
             }
 
         }
-        public string GetOutputList() { return queue==null?"":queue.OutputList; }
+        public List<int> GetOutputString() { return queue.FinalList; }
     }
 
     class Queue {
@@ -31,35 +32,50 @@ namespace DataStructure {
             this.Size = size;
             QueueList = new List<Package>();
         }
-        public string OutputList = String.Empty;
         public List<Package> QueueList { get; set; }
+        public List<int> FinalList { get; set; }
         public int Size { get; set; }
         public int OccupiedSize { get; set; } = 0;
         public int WorkingTime { get; set; } = 0;
-        public void AddPackage(Package package) {
-            if (Size - OccupiedSize > 0) {
-                OccupiedSize++;
-                QueueList.Add(package);
-                WorkingTime = package.ArrivalTime > WorkingTime ? package.ArrivalTime : WorkingTime;
-                OutputList = OutputList + WorkingTime+Environment.NewLine;
 
-            } else if (Size - OccupiedSize == 0) {
-                if (package.ArrivalTime == QueueList[Size - 1].ArrivalTime) {
-                    OutputList = OutputList + Environment.NewLine + "-1";
-                } else if(package.ArrivalTime > QueueList[Size - 1].ArrivalTime) {
-                    if (package.ArrivalTime == QueueList[0].DurationTime) {
-                        WorkingTime += QueueList[0].DurationTime;
-                        OutputList = OutputList + Environment.NewLine + WorkingTime.ToString();                        
-                        QueueList.RemoveAt(0);
-                        QueueList.Add(package);
-                    } else {
-                        OutputList = OutputList + Environment.NewLine + "-1";
-                    }
-
-
+        //1
+        public void ProcessPackage() {
+            if (QueueList.Count > 0) {
+                Package package = QueueList[0];
+                if (WorkingTime >= package.DurationTime) {
+                    QueueList.RemoveAt(0);
+                    FinalList.Add(WorkingTime);
+                    // package.ProcessStartTime = WorkingTime;
                 }
-
             }
+
+        }
+        public void AddPackage(Package package) {
+            if (Size - OccupiedSize > 0 && package.ProcessStartTime != -1) {
+                OccupiedSize++;
+                WorkingTime = WorkingTime + package.ArrivalTime;
+                QueueList.Add(package);
+            } else {
+                package.ProcessStartTime = -1;
+            }
+
+            //} else if (Size - OccupiedSize == 0) {
+            //    if (package.ArrivalTime == QueueList[Size - 1].ArrivalTime) {
+            //        EveryPackageProcessStartTime = EveryPackageProcessStartTime + "-1" + Environment.NewLine;
+            //    } else if (package.ArrivalTime > QueueList[Size - 1].ArrivalTime) {
+            //        if (package.ArrivalTime == QueueList[0].DurationTime) {
+            //            WorkingTime += QueueList[0].DurationTime;
+            //            EveryPackageProcessStartTime = EveryPackageProcessStartTime + Environment.NewLine + WorkingTime.ToString();
+            //            QueueList.RemoveAt(0);
+            //            QueueList.Add(package);
+            //        } else {
+            //            EveryPackageProcessStartTime = EveryPackageProcessStartTime + Environment.NewLine + "-1";
+            //        }
+
+
+            //    }
+
+            //}
         }
 
     }
@@ -70,8 +86,10 @@ namespace DataStructure {
         public Package(int arrivalTime, int durationTime) {
             this.ArrivalTime = arrivalTime;
             this.DurationTime = durationTime;
+            this.ProcessStartTime = arrivalTime;
         }
         public int ArrivalTime { get; set; }
         public int DurationTime { get; set; }
+        public int ProcessStartTime { get; set; }
     }
 }
--- DataStructure/Tests/Tests.cs
+++ DataStructure/Tests/Tests.cs
@@ -6,7 +6,7 @@ namespace Tests {
         public void Setup() {
         }
 
-        
+
         public void StackTests() {
             using (var tw = new StringWriter()) {
                 Console.SetOut(tw);
@@ -29,11 +29,11 @@ namespace Tests {
                 Program.Main();
                 string[] numberList = ("5" + Environment.NewLine + "4 -1 4 1 1").Split(Environment.NewLine);
                 TreeHeight treeHeight = new TreeHeight(Convert.ToInt32(numberList[0]), numberList[1]);
-              //  Assert.That(treeHeight.GetHeight()==3);
+                //  Assert.That(treeHeight.GetHeight()==3);
 
                 numberList = ("5" + Environment.NewLine + "-1 0 4 0 3").Split(Environment.NewLine);
                 treeHeight = new TreeHeight(Convert.ToInt32(numberList[0]), numberList[1]);
-               // Assert.That(treeHeight.GetHeight() == 4);
+                // Assert.That(treeHeight.GetHeight() == 4);
 
                 numberList = ("10" + Environment.NewLine + "9 7 5 5 2 9 9 9 2 -1").Split(Environment.NewLine);
                 treeHeight = new TreeHeight(Convert.ToInt32(numberList[0]), numberList[1]);
@@ -46,44 +46,76 @@ namespace Tests {
             using (var tw = new StringWriter()) {
                 Console.SetOut(tw);
                 Program.Main();
-                //public StautsOfNetPackages(string size_number, string[] packages) {
+
+                //-------------1------------------
                 string size_number = "1 1";
                 List<string> packages = new List<string>();
                 packages.Add("0 0");
+
                 StautsOfNetPackages queue = new StautsOfNetPackages(size_number, packages);
-                var output = queue.GetOutputList();
-                Assert.That(output == "0");
 
+                //actual
+                List<int> valuesActual = queue.GetOutputString();
+                string outputActual = String.Join(Environment.NewLine, valuesActual);
+
+                //expected
+                List<int> valuesExpected = new List<int>() { 0 };
+                string outputExpected = String.Join(Environment.NewLine, valuesExpected);
+
+                Assert.That(outputActual == outputExpected);
+
+                //-------------2------------------
                 size_number = "1 2";
                 packages = new List<string>();
                 packages.Add("0 1");
-                packages.Add("1 1");                
+                packages.Add("1 1");
                 queue = new StautsOfNetPackages(size_number, packages);
-                output = queue.GetOutputList();
-                Assert.That(output == "0"+Environment.NewLine+"1");
 
-                size_number = "1 2";
-                packages = new List<string>();
-                packages.Add("0 1");
-                packages.Add("0 1");
-                queue = new StautsOfNetPackages(size_number, packages);
-                output = queue.GetOutputList();
-                Assert.That(output == "0" + Environment.NewLine + "-1");
+                //actual
+                valuesActual = queue.GetOutputString();
+                outputActual = String.Join(Environment.NewLine, valuesActual);
 
+                //expected
+                valuesExpected = new List<int>() { 0, 1 };
+                outputExpected = String.Join(Environment.NewLine, valuesExpected);
 
+                Assert.That(outputActual == outputExpected);
+
+                //-------------3------------------
                 size_number = "1 0";
-                packages = null;// ("0 1" + Environment.NewLine + "0 1").Split(Environment.NewLine);
+                packages = null;
                 queue = new StautsOfNetPackages(size_number, packages);
-                output = queue.GetOutputList();
-                Assert.That(output == "");
-                //numberList = ("5" + Environment.NewLine + "-1 0 4 0 3").Split(Environment.NewLine);
-                //treeHeight = new TreeHeight(Convert.ToInt32(numberList[0]), numberList[1]);
-                //// Assert.That(treeHeight.GetHeight() == 4);
 
-                //numberList = ("10" + Environment.NewLine + "9 7 5 5 2 9 9 9 2 -1").Split(Environment.NewLine);
-                //treeHeight = new TreeHeight(Convert.ToInt32(numberList[0]), numberList[1]);
-                //var height = treeHeight.GetHeight();
-                //Assert.That(height == 4);
+                //actual
+                valuesActual = queue.GetOutputString();
+                outputActual = String.Join(Environment.NewLine, valuesActual);
+
+                //expected
+                valuesExpected = new List<int>() { };
+                outputExpected = String.Join(Environment.NewLine, valuesExpected);
+
+                Assert.That(outputActual == outputExpected);
+
+                //-------------4------------------
+                size_number = "2 5";
+                packages = new List<string>();
+                packages.Add("2 9");
+                packages.Add("4 8");
+                packages.Add("10 9");
+                packages.Add("15 2");
+                packages.Add("19 1");
+                queue = new StautsOfNetPackages(size_number, packages);
+
+                //actual
+                valuesActual = queue.GetOutputString();
+                outputActual = String.Join(Environment.NewLine, valuesActual);
+
+                //expected
+                valuesExpected = new List<int>() { 2, 11, -1, 19, 21 };
+                outputExpected = String.Join(Environment.NewLine, valuesExpected);
+
+                Assert.That(outputActual==outputExpected);
+
             }
         }
     }
