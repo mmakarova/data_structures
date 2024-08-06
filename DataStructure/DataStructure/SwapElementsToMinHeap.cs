19969944e0a59f71289d0429f57d91310c6e8718
mmakarova <makarova.marina.andreevna@gmail.com>
2024-08-06 20:43:35 +0300

    SwapElementsToMinHeap the first commit

---
+++ DataStructure/DataStructure/SwapElementsToMinHeap.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure {
+    public class SwapElementsToMinHeap {
+        public SwapElementsToMinHeap(List<int> array)
+        {
+            
+        }
+    }
+}
